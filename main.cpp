99607f66b2d4e2bc79704899f771dd0448d69f70
nurmukhamed.ubaidullayev <nurmukhamed.ubaidullayev@giant.ai>
2022-12-25 20:52:37 +0600

    apply sphere class

--- main.cpp
+++ main.cpp
@@ -2,30 +2,18 @@
 #include "vec3.h"
 #include "color.h"
 #include "ray.h"
-
-double hit_sphere(const point3& center, double radius, const ray& r){
-    vec3 oc = r.origin() - center;
-
-    auto a = r.direction().length_squared();
-    auto half_b = dot(r.direction(), oc);
-    auto c = oc.length_squared() - radius * radius;
-
-    auto discriminant = half_b*half_b - a*c;
-    if (discriminant < 0)
-        return -1;
-    return (-half_b - sqrt(discriminant))/(a);
-}
+#include "sphere.h"
 
 
-color ray_color(const ray& r){
-    point3 center = point3(0,0,-1);
-    auto t = hit_sphere(center, 0.5, r);
-    if (t > 0){
-        vec3 N = unit_vector(r.at(t) - center);
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
-    }
+
+color ray_color(const ray& r, const sphere& sph){
+    hit_record hr;
+
+    if (sph.hit(r, 1.0, 1.5, hr))
+        return 0.5 * color(hr.normal.x() + 1, hr.normal.y() + 1, hr.normal.z() + 1);
+    
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y()+1.0);
+    auto t = 0.5 * (unit_direction.y()+1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -48,13 +36,15 @@ int create_image(){
     // Render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
+    sphere sph(vec3(0,0,-1), 0.5);
+    
     for (int j = image_height - 1; j >= 0; --j){
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i){
             auto u = double(i) / (image_width - 1);
             auto v = double(j) / (image_height - 1);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, sph);
             write_color(std::cout, pixel_color);
         }
     }
