8df198981a3e2f11c3a445e9dc41717263334c37
nurmukhamed.ubaidullayev <nurmukhamed.ubaidullayev@giant.ai>
2022-12-25 16:27:41 +0600

    created class vec3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include "vec3.h"
 
-int main(){
+int create_image(){
     // Image
     const int image_width = 256;
     const int image_height = 256;
@@ -9,6 +10,7 @@ int main(){
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j){
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i){
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -21,4 +23,14 @@ int main(){
         }
     }
 
+    std::cerr << "\nDone\n";
+
+    return 1;
+}
+
+int main(){
+    // create_image();
+    vec3 v1(1,2,3), v2(0, 1, 2);
+    std::cout << (-v1).length_squared() << std::endl;
+    return 1;
 }
