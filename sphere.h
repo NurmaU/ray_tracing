6c425cae00e8293e94104a52d4695868fde3baa4
nurmukhamed.ubaidullayev <nurmukhamed.ubaidullayev@giant.ai>
2022-12-25 21:01:21 +0600

    front face

--- sphere.h
+++ sphere.h
@@ -36,10 +36,11 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
         if (root < t_min || t_max < root)
             return false;
     }
-    
+
     rec.t = root;
     rec.p = r.at(root);
-    rec.normal = (rec.p - center) / radius;
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
 
     return true;
 }
