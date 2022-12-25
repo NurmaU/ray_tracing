5a68eb1addc178bcb51b7f1db9349e53341a01c9
nurmukhamed.ubaidullayev <nurmukhamed.ubaidullayev@giant.ai>
2022-12-25 21:26:41 +0600

    added world example

--- hittable_list.h
+++ hittable_list.h
@@ -34,10 +34,10 @@ class hittable_list: public hittable{
 bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const{
     hit_record temp_rec;
     bool hit_anything = false;
-    auto closest_so_far = t_max; // TODO not used anywhere
+    auto closest_so_far = t_max;
 
     for(const auto& object: objects){
-        if (object->hit(r, t_min, t_max, temp_rec)){
+        if (object->hit(r, t_min, closest_so_far, temp_rec)){
             hit_anything = true;
             closest_so_far = temp_rec.t;
             rec = temp_rec;
