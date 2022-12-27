796b562b07f9997ebab9c440642bf4c5389c4886
nurmukhamed.ubaidullayev <nurmukhamed.ubaidullayev@giant.ai>
2022-12-27 14:36:57 +0600

    camera pose

--- camera.h
+++ camera.h
@@ -11,23 +11,26 @@ class camera {
     vec3 vertical;
 
    public:
-    camera(double vfov, double aspect_ratio) {
+    camera(point3 lookfrom, point3 lookat, vec3 vup, double vfov,
+           double aspect_ratio) {
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
         auto viewport_height = 2.0 * h;
         auto viewport_width = viewport_height * aspect_ratio;
-        auto focal_length = 1.0;
+        // auto focal_length = 1.0;
+        auto w = unit_vector(lookfrom - lookat);
+        auto u = unit_vector(cross(vup, w));
+        auto v = unit_vector(cross(w, u));
 
-        origin = point3(0, 0, 0);
-        horizontal = vec3(viewport_width, 0.0, 0.0);
-        vertical = vec3(0.0, viewport_height, 0.0);
-        lower_left_corner =
-            origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+        origin = lookfrom;
+        horizontal = viewport_width * u;
+        vertical = viewport_height * v;
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
     }
 
-    ray get_ray(double u, double v) {
+    ray get_ray(double s, double t) {
         return ray(origin,
-                   lower_left_corner + u * horizontal + v * vertical - origin);
+                   lower_left_corner + s * horizontal + t * vertical - origin);
     }
 };
 
